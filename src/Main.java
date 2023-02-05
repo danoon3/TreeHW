6db1132dd74ce819091f3a43b1db57c140922663
Denis Chernikov <danoone58@gmail.com>
2023-02-05 20:43:33 +0300

    Resolved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> set = new ArrayList<>();
+        set.add(new Person("Denis", "Chernikov One  ", 26));
+        set.add(new Person("Irina", "Polyakova Chernikova one ", 25));
+        set.add(new Person("Olga", "Zimenkova ", 39));
+        set.add(new Person("Sergey", "Chernikov one two ", 40));
+
+        Collections.sort(set, new PersonNameComparator(3));
+
+        for (Person p: set) {
+            System.out.println(p.getSurname() + " " + p.getAge());
+        }
+
+
     }
 }
---
+++ src/PersonNameComparator.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+
+public class PersonNameComparator implements Comparator<Person> {
+    private int countOfSurnameWords;
+
+    public PersonNameComparator(int countOfSurnameWords) {
+        this.countOfSurnameWords = countOfSurnameWords;
+    }
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        String firstSurname = p1.getSurname();
+        String secondSurname = p2.getSurname();
+
+        String[] firstWords = firstSurname.split("\\P{IsAlphabetic}+");
+        String[] secondWords = secondSurname.split("\\P{IsAlphabetic}+");
+
+        if (firstWords.length <= countOfSurnameWords && secondWords.length <= countOfSurnameWords) {
+
+            if (firstWords.length > secondWords.length) {
+                return -1;
+            } else if (firstWords.length < secondWords.length) {
+                return 1;
+            } else {
+                return Integer.compare(p1.getAge(), p2.getAge());
+            }
+        } else {
+            return Integer.compare(p1.getAge(), p2.getAge());
+        }
+    }
+}
